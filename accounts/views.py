3d5949b29d6f4ab9891949928c7577a4406af170
guirlviana <girlviana@gmail.com>
2021-07-15 22:02:52 -0300

    adicionado login, logout e dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,36 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.core.validators import validate_email
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method != "POST":
+        return render(request, 'accounts/login.html')
+
+    for valor in request.POST.values(): # validando campos vazios
+        if not valor:
+            messages.error(request, "Preencha os campos!")
+            return render(request, 'accounts/login.html')
+
+    usuario = request.POST.get('usuario')
+    senha = request.POST.get('senha')
+
+    user = auth.authenticate(request, username=usuario, password=senha)
+    if not user:
+        messages.error(request, "Login inválido")
+        return render(request, 'accounts/login.html') 
+    else:
+        auth.login(request, user)
+        messages.success(request, "Login feito com sucesso")
+        return redirect('dashboard')
+        
 
 def logout(request):
-    return render(request, 'accounts/logout.html')
+    auth.logout(request)
+    return redirect('login')
 
-def cadastro(request):
+def cadastro(request):      
     if request.method != 'POST':
         return render(request, 'accounts/cadastro.html')
 
@@ -59,6 +80,6 @@ def cadastro(request):
     user.save()
     return redirect('login')
     
-
+@login_required(redirect_field_name='login')
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
